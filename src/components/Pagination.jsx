4de376b58ff64bce554baf85c2000a281c35b240
TunarshBee <rafiubello4real@gmail.com>
2022-12-18 12:28:23 +0100

    Tried for users in pagination component

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,6 +1,19 @@
+// import axios from "axios";
 import React from "react";
+// Don't forget to add the useState and useEffect hooks though
 
 const Pagination = ({ postsperpage, totalposts, paginate }) => {
+    // const [users, setUsers] = useState([])
+    // useEffect(() => {
+    //   const getUsers = async (e)=>{
+    //     const res = await axios.get("https://jsonplaceholder.typicode.com/users")
+    //     setUsers(res.data)
+    //   }
+    
+    //   getUsers()
+    // }, [])
+    // console.log(users)
+    
   const pageNumber = [];
   for (let i = 1; i <= Math.ceil(totalposts / postsperpage); i++) {
     pageNumber.push(i);
